332e2bb48d2743c3e5ee742861f73209f78fc716
Hugo Chinga G <hugo.chinga.g@gmail.com>
2021-02-21 14:05:50 -0300

    version con lista de últimas actualizaciones

--- app/controllers/recursos_controller.rb
+++ app/controllers/recursos_controller.rb
@@ -3,6 +3,8 @@ class RecursosController < ApplicationController
   before_action :inicia_session
   before_action :carga_temas_ayuda
 
+  helper_method :sort_column, :sort_direction
+
 	def index
 		@coleccion = {}
 		@coleccion['administradores'] = Administrador.all
@@ -10,6 +12,19 @@ class RecursosController < ApplicationController
 	end
 
 	def home
-		@coleccion = TemaAyuda.where(tipo: 'inicio').order(:orden)
+		@coleccion = {}
+		@coleccion['tema_ayudas'] = TemaAyuda.where(tipo: 'inicio').order(:orden)
+		@coleccion['publicaciones'] = Publicacion.where(estado: 'publicada').order(created_at: :asc).last(10)
 	end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+
+    def sort_column
+      Publicacion.column_names.include?(params[:sort]) ? params[:sort] : "Author"
+    end
+    
+    def sort_direction
+      %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    end
 end
