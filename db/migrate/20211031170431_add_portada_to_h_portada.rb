e537d9d7d4834a6edb10e07d5a75ca410a4720e8
Hugo Chinga G <hugo.chinga.g@gmail.com>
2021-11-02 13:05:55 -0300

    lkjhl

--- db/migrate/20211031170431_add_portada_to_h_portada.rb
+++
@@ -1,9 +0,0 @@
-class AddPortadaToHPortada < ActiveRecord::Migration[5.2]
-  def change
-    add_column :h_portadas, :portada, :string
-    remove_column :h_portadas, :imagen, :string
-    remove_column :h_portadas, :credito_imagen, :string
-    
-    add_index :h_portadas, :portada
-  end
-end
