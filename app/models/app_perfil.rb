50585c17c93f67f803d5eff56c900e223082b454
Hugo Chinga G <hugo.chinga.g@gmail.com>
2023-02-22 12:02:06 -0300

    arreglo ingreso de categorías

--- app/models/app_perfil.rb
+++ app/models/app_perfil.rb
@@ -11,13 +11,14 @@ class AppPerfil < ApplicationRecord
 	has_many :app_mejoras
 
 	has_many :carpetas
+	has_many :cargas
+	has_many :contribuciones, class_name: 'Publicacion'
+	has_many :aportes, foreign_key: 'perfil_id', class_name: 'Categoria'
 
 	has_many :equipos, foreign_key: 'app_administrador_id'
 
 	has_many :per_equipos
 	has_many :participaciones, through: :per_equipos, source: :equipo
 
-	has_many :contribuciones, class_name: 'Publicacion'
 
-	has_many :cargas
 end
