52355b7a2ad4abc87169b8a2c8671ca64f881ea0
Hugo Chinga G <hugo.chinga.g@gmail.com>
2023-09-12 12:45:51 -0300

    jkhk

--- app/models/filo_sinonimo.rb
+++ app/models/filo_sinonimo.rb
@@ -6,7 +6,15 @@ class FiloSinonimo < ApplicationRecord
 	has_many :filo_especies, through: :filo_esp_sinos
 
 	def conflicto?
-		( self.filo_especies.count > 1 ) or ( self.especie.blank? ? false : self.especie.filo_especie.present? )
+		self.multiple? or self.sinonimo_especie?
+	end
+
+	def multiple?
+		self.filo_especies.count > 1
+	end
+
+	def sinonimo_especie?
+		self.especie.blank? ? false : self.especie.filo_especie.present?
 	end
 
 end
