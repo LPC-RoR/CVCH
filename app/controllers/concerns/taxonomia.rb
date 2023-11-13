5fca8d9ac330b436c7ce733c75ca12604907d047
Hugo Chinga G <hugo.chinga.g@gmail.com>
2023-11-13 13:51:50 -0300

    hgkjg

--- app/controllers/concerns/taxonomia.rb
+++ app/controllers/concerns/taxonomia.rb
@@ -35,30 +35,30 @@ module Taxonomia
 				end
 			end
 		else
-			nombre_especie = especie.especie.downcase.strip
-			filo_especie = FiloEspecie.find_by(filo_especie: nombre_especie)
-			if filo_especie.blank?
-				palabras = especie.especie.downcase.split(' ')
-				genero = FiloElemento.find_by(filo_elemento: palabras[0])
-				if genero.blank?
-					orden_id = FiloOrden.find_by(filo_orden: 'Género').id
-					genero = FiloElemento.create(filo_orden_id: orden_id, filo_elemento: palabras[0], revisar: true)
-				end
-				if palabras.length == 2
-					filo_especie = genero.filo_especies.create(filo_especie: nombre_especie)
-					filo_especie.especies << especie
-				else
-					s_filo_especie = "#{palabras[0]} #{palabras[1]}"
-					filo_especie = FiloEspecie.find_by(filo_especie: s_filo_especie)
-					filo_especie = genero.filo_especies.create(filo_especie: s_filo_especie) if filo_especie.blank?
+#			nombre_especie = especie.especie.downcase.strip
+#			filo_especie = FiloEspecie.find_by(filo_especie: nombre_especie)
+#			if filo_especie.blank?
+#				palabras = especie.especie.downcase.split(' ')
+#				genero = FiloElemento.find_by(filo_elemento: palabras[0])
+#				if genero.blank?
+#					orden_id = FiloOrden.find_by(filo_orden: 'Género').id
+#					genero = FiloElemento.create(filo_orden_id: orden_id, filo_elemento: palabras[0], revisar: true)
+#				end
+#				if palabras.length == 2
+#					filo_especie = genero.filo_especies.create(filo_especie: nombre_especie)
+#					filo_especie.especies << especie
+#				else
+#					s_filo_especie = "#{palabras[0]} #{palabras[1]}"
+#					filo_especie = FiloEspecie.find_by(filo_especie: s_filo_especie)
+#					filo_especie = genero.filo_especies.create(filo_especie: s_filo_especie) if filo_especie.blank?
 
-					sub_especie = FiloEspecie.create(filo_especie: nombre_especie)
-					filo_especie.children << sub_especie
-					sub_especie.especies << especie
-				end
-			else
-				filo_especie.especies << especie
-			end
+#					sub_especie = FiloEspecie.create(filo_especie: nombre_especie)
+#					filo_especie.children << sub_especie
+#					sub_especie.especies << especie
+#				end
+#			else
+#				filo_especie.especies << especie
+#			end
 		end
 	end
 end
