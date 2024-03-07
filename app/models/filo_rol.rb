ef68bd1ebee1708c6fe0be7112fa2cbbab72d063
Hugo Chinga G <hugo.chinga.g@gmail.com>
2024-03-06 23:54:27 -0300

    version con manejo de cita de interaccion

--- app/models/filo_rol.rb
+++ app/models/filo_rol.rb
@@ -1,4 +1,12 @@
 class FiloRol < ApplicationRecord
 	belongs_to :filo_especie
 	belongs_to :filo_interaccion
+
+	def primer_rol?
+		self.filo_rol == self.filo_interaccion.primer_rol.filo_rol
+	end
+
+	def otro_rol
+		self.filo_interaccion.filo_roles.where.not(filo_rol: self.filo_rol).first
+	end
 end
