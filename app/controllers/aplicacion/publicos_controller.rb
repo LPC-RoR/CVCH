1681d15629b73c3091e68248de57f6dac50557e9
Hugo A. Chinga Guerrero <hugo.chinga.g@gmail.com>
2024-05-06 17:16:30 -0400

    llkjh

--- app/controllers/aplicacion/publicos_controller.rb
+++ app/controllers/aplicacion/publicos_controller.rb
@@ -105,7 +105,7 @@ class Aplicacion::PublicosController < ApplicationController
         set_tabla('agregados-filo_sinonimos', @objeto.fs_agregados, false)
 
         filo_interacciones_ids = @objeto.filo_roles.map {|fr| fr.filo_interaccion_id}.uniq
-        set_tabla('filo_interacciones', FiloInteracion.where(id: filo_interacciones_ids), false)
+        set_tabla('filo_interacciones', FiloInteraccion.where(id: filo_interacciones_ids), false)
 
         set_tabla('filo_actualizaciones', @objeto.filo_actualizaciones.order(updated_at: :desc), false)
 
